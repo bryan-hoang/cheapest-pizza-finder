cf442d3fce8daa52347a8ef26477c8d84773cb7a
alcatrazEscapee <alex@molleroneill.com>
2018-11-24 11:53:24 -0500

    Added selenium web driver + basic web scraping backend

--- script.py
+++ script.py
@@ -1,16 +1,28 @@
 # this is a test script
 
+import time
+from selenium import webdriver
 from flask import Flask, render_template
+
+# setup
 app = Flask(__name__)
 
+# main page
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# results page
 @app.route('/scrape/<address>')
 def scrape(address):
 	# This will do the actual web scraping
 	
 	# This is some thing
+	driver = webdriver.Chrome()
+	driver.get('chrome://settings/')
+
+	time.sleep(3);
+	
+	driver.quit();
 	
 	return render_template('scrape.html', template_address = address)
