a4c7e79b1cbf22f74a430c79bd4ee9ce0cef2d59
alcatrazEscapee <alex@molleroneill.com>
2018-11-24 14:45:04 -0500

    Merged distance check into the main script.py

--- script.py
+++ script.py
@@ -27,10 +27,16 @@ def scrape(address, radius):
 	driver.get('https://www.yellowpages.ca/search/si/1/pizza/%s' % formatted_address)
 
 	boxes = driver.find_elements_by_class_name('listing__content__wrapper')
-	amount_to_check = 4
+	amount_to_check = 2
 	for box in boxes:
 		try:
 			box_element = box.find_element_by_tag_name('h3')
+			box_dist = box.find_element_by_class_name('listing__distance').text
+			box_dist = box_dist[:-2]
+			box_dist = float(box_dist)
+			if box_dist > float(radius):
+				continue
+			
 			box_name = box_element.text
 			box_link = box_element.find_element_by_tag_name('a').get_attribute('href')
 			#use a second driver?
