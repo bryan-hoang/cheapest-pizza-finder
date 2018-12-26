3b7fea4c5ba926ce0c4be66982fa7d299828635d
Bryan Hoang <Bryan.C.Hoang@gmail.com>
2018-12-25 21:42:54 -0500

    Format

--- src/price_scraper.py
+++ src/price_scraper.py
@@ -6,7 +6,7 @@
 def get_price(driver, website):
 
     driver.get(website)
-    output = 'Stuff:\n\n'
+    output = "Stuff:\n\n"
 
     # load the current active menu
     # menu = driver.find_element_by_xpath("//*[@class='menu__content
@@ -17,29 +17,28 @@ def get_price(driver, website):
 
     # fallback
     rate_element = driver.find_element_by_xpath(
-        "//*[@class='merchant__item merchant__item" +
-        "--inline merchant__item--rates']")
-    rate_list_element = rate_element.find_element_by_tag_name('li')
+        "//*[@class='merchant__item merchant__item" + "--inline merchant__item--rates']"
+    )
+    rate_list_element = rate_element.find_element_by_tag_name("li")
     rate = rate_list_element.text
     # output += 'The rates are: %s' % rate
 
     for item in menu_items:
         try:
-            item_title = item.find_element_by_class_name(
-                'food-menu--title').text
-            output += 'Item: %s\n' % item_title
+            item_title = item.find_element_by_class_name("food-menu--title").text
+            output += "Item: %s\n" % item_title
         except Exception:
-            output += 'Item: ???\n'
+            output += "Item: ???\n"
             pass
 
         try:
-            output += 'Price: '
-            item_price = item.find_element_by_class_name('food-menu--price')
-            for price_element in item_price.find_elements_by_tag_name('span'):
-                output += '%s, ' % price_element.get_attribute('innerHTML')
+            output += "Price: "
+            item_price = item.find_element_by_class_name("food-menu--price")
+            for price_element in item_price.find_elements_by_tag_name("span"):
+                output += "%s, " % price_element.get_attribute("innerHTML")
         except Exception:
-            output += 'FAIL\n'
+            output += "FAIL\n"
             pass
-        output += '\n\n'
+        output += "\n\n"
 
     return rate
