2e062a38ae832f8069e729f958c3fe00b84c52b9
Bryan Hoang <bryan.hoang@queensu.ca>
2024-12-04 10:48:33 -0500

    refactor: update project to work with uv

--- cheapest_pizza_finder/price_scraper.py
+++ cheapest_pizza_finder/price_scraper.py
@@ -1,10 +1,9 @@
 # simple util file to get the price of a website using selenium
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
 
-# from selenium import webdriver
 
-
-def get_price(driver, website):
-
+def get_price(driver: WebDriver, website):
     driver.get(website)
     output = "Stuff:\n\n"
 
@@ -13,19 +12,20 @@ def get_price(driver, website):
     # jsMenuContent']")
 
     # find all items in the menu
-    menu_items = driver.find_elements_by_class_name("food-menu--item")
+    menu_items = driver.find_elements(By.CLASS_NAME, "food-menu--item")
 
     # fallback
-    rate_element = driver.find_element_by_xpath(
-        "//*[@class='merchant__item merchant__item" + "--inline merchant__item--rates']"
+    rate_element = driver.find_element(
+        By.XPATH,
+        "//*[@class='merchant__item merchant__item--inline merchant__item--rates']",
     )
-    rate_list_element = rate_element.find_element_by_tag_name("li")
+    rate_list_element = rate_element.find_element(By.TAG_NAME, "li")
     rate = rate_list_element.text
     # output += 'The rates are: %s' % rate
 
     for item in menu_items:
         try:
-            item_title = item.find_element_by_class_name("food-menu--title").text
+            item_title = item.find_element(By.CLASS_NAME, "food-menu--title").text
             output += "Item: %s\n" % item_title
         except Exception:
             output += "Item: ???\n"
@@ -33,8 +33,8 @@ def get_price(driver, website):
 
         try:
             output += "Price: "
-            item_price = item.find_element_by_class_name("food-menu--price")
-            for price_element in item_price.find_elements_by_tag_name("span"):
+            item_price = item.find_element(By.CLASS_NAME, "food-menu--price")
+            for price_element in item_price.find_elements(By.TAG_NAME, "span"):
                 output += "%s, " % price_element.get_attribute("innerHTML")
         except Exception:
             output += "FAIL\n"
